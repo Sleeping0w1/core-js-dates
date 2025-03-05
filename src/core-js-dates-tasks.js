c7c4cff90817105744478ac7730c3a52e564c6db
Sleeping0w1 <1033351@gmail.com>
2025-03-05 23:13:46 +0300

    feat: solution for isLeapYear

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -279,8 +279,11 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getFullYear();
+  if (year % 100 === 0 && year % 400 !== 0) return false;
+  if (year % 4 === 0) return true;
+  return false;
 }
 
 module.exports = {
